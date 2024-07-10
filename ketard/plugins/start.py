0bea5df6bfb7c91b9b85bd70543bdb38ae547039
suphiozturk8 <suphiozturk8@gmail.com>
2024-07-10 12:36:22 +0300

    Better '/help' and '/start'  message

--- ketard/plugins/start.py
+++ ketard/plugins/start.py
@@ -2,24 +2,38 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from ketard import VERSION, my_filters
-from ketard.config import DataConfig, BotConfig
+from ketard.config import BotConfig
 from ketard.logger.logging import LOGGER
 
 
 @Client.on_message(filters.command(["start"]) & my_filters.is_user_spamming())
 async def handle_start_command(_, message: Message):
-    start_message = f"""
-Hello! {message.from_user.mention}
-I am the `{BotConfig.BOT_NAME}` bot.\n
-General Commands:
-- `/help` : Shows the help message.
-- `/status` or `/boardinfo` : Displays system information.
-- `/about` : Shows information about the bot.
-- `/vidsum` : Summarizes the video. (WIP)
-- `/textsum` : Summarizes the text. (experimental)
-- `{", ".join("/{cmd}".format(cmd=cmd) for cmd in DataConfig.GEN_COMMANDS)}` : Generates a response based on the given command.\n
-For questions and feedback, you can message the admins.
+    start_text = """
+**Hello, {USER}!**
+
+Welcome to {BOT_NAME}! I'm here to assist you with various tasks. Here are a few things you can do with me:
+
+- Ask questions on any topic.
+- Get code samples and snippets.
+- Summarize YouTube videos.
+- Describe images and more (work in progress)!
+
+To get started, simply type a command or ask me a question.
+For a list of available commands, type `/help`.
+
+Happy exploring!
+
+[Github Repo](https://github.com/ket0x4/ketard-ai/tree/dev)
+[Contributors](https://github.com/ket0x4/ketard-ai/graphs/contributors)
     """
 
-    await message.reply_text(start_message, quote=True)
+    await message.reply_text(
+        text=start_text.format(
+            USER=message.from_user.mention,
+            BOT_NAME=BotConfig.BOT_NAME,
+        ),
+        quote=True,
+        disable_web_page_preview=True
+    )
+    
     LOGGER(__name__).info("Start command invoked.")
