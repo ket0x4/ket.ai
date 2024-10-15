16141560943109ed11210ebf1020e6c4cd25505d
ibrahim yöndemli <ket0x4@pm.me>
2024-10-15 15:07:40 +0300

    remove unused imports and fıormatting

--- ketard.py
+++ ketard.py
@@ -5,7 +5,6 @@ import random
 import psutil
 import os, time
 from pyrogram import Client, filters, enums, idle
-from youtube_transcript_api import YouTubeTranscriptApi
 import asyncio
 
 # Load JSON data
@@ -18,7 +17,7 @@ except (FileNotFoundError, json.JSONDecodeError) as e:
 
 # Default configurations
 DEBUG = False
-VERSION = "legacy-py"
+VERSION = "legacy"
 ALLOWED_CHATS = data.get("groups", [])
 ALLOWED_USERS = data.get("users", [])
 ADMINS = data.get("admins", [])
@@ -82,10 +81,10 @@ async def llama_completion(prompt):
     url = API_URL + "/completion"
     headers = {"Content-Type": "application/json"}
     pre_prompt = ""
-    prompt = "Q:" + pre_prompt + prompt
+    prompt = pre_prompt + prompt
     payload = {
         "prompt": prompt,
-        "n_predict": 1024,
+        "n_predict": 2048,
     }
     async with aiohttp.ClientSession() as session:
         async with session.post(url, json=payload, headers=headers) as response:
@@ -93,33 +92,49 @@ async def llama_completion(prompt):
             # Parse response
             if "content" in response_json:
                 result = response_json["content"]
-                #print(f"Response: {result}")
+                # print(f"Response: {result}")
                 return result
             else:
                 logging.error("Unexpected response format: %s", response_json)
                 return "`Failed to Process prompt`"
-            
+
+
 # Telegram Bot prompt Command
 @bot.on_message(filters.command(GEN_COMMANDS))
 async def handle_ket_command(bot, message):
     if not await llama_health_check():
         await message.reply_text("Cannot reach backend.", quote=True)
-        logging.error("UserID: {message.from_user.id} tried to use /ket command but backend is unreachable.")
+        logging.error(
+            f"UserID: {message.from_user.id} tried to use /ket command but backend is unreachable."
+        )
         return
+
     chat_id, user_id = str(message.chat.id), str(message.from_user.id)
-    if user_id not in map(str, ADMINS) and chat_id not in map(str, ALLOWED_CHATS) and user_id not in map(str, ALLOWED_USERS):
+    if (
+        user_id not in map(str, ADMINS)
+        and chat_id not in map(str, ALLOWED_CHATS)
+        and user_id not in map(str, ALLOWED_USERS)
+    ):
         await message.reply_text("You are not authorized to use this bot.", quote=True)
-        logging.warning(f"Unauthorized user: {message.from_user.id} tried to use /ket command.")
+        logging.warning(
+            f"Unauthorized user: {message.from_user.id} tried to use /ket command."
+        )
         return
+
     if len(message.command) < 2:
         await message.reply_text("Please provide a prompt.", quote=True)
-        logging.warning(f"UserID: {message.from_user.id} invoked /ket command without a prompt.")
+        logging.warning(
+            f"UserID: {message.from_user.id} invoked /ket command without a prompt."
+        )
         return
+
     prompt = " ".join(message.command[1:])
     completion = await llama_completion(prompt)
     await message.reply_text(completion, quote=True)
-    logging.info(f"UserID: {message.from_user.id} invoked /ket command with prompt: {prompt}")
-    
+    logging.info(
+        f"UserID: {message.from_user.id} invoked /ket command with prompt: {prompt}"
+    )
+
 
 # System Status
 def get_system_stats():
@@ -146,6 +161,7 @@ def get_system_stats():
 
     return f"""
 **System Status**
+Version: `{VERSION}`
 Board: `{board_name}`
 OS: `{os_name}`
 CPU Usage: `{cpu_usage}%`
@@ -154,11 +170,14 @@ CPU Temperature: `{cpu_temp}`
 Backend: `Llama.cpp`
 LLM Model: `{model_name}`
 """
+
+
 # Telegram Bot /htatus Command
 @bot.on_message(filters.command(["status", "boardinfo"]))
 async def handle_status_info_command(bot, message):
     await message.reply_text(get_system_stats(), quote=True)
     logging.info(f"UserID: {message.from_user.id} invoked /status command.")
+
 
 # Telegram Bot /help Command
 @bot.on_message(filters.command(["help"]))
